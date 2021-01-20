8c8d556bb5e23441c7fcc29f534d0b27c90631b8
rustikella <37469475+rustikella@users.noreply.github.com>
2021-01-20 22:59:30 +0100

    Update run.sh

--- jmeter/run.sh
+++ jmeter/run.sh
@@ -10,4 +10,4 @@ echo "$ROOTPATH"
 # Finally run
 docker stop $NAME > /dev/null 2>&1
 docker rm $NAME > /dev/null 2>&1
-docker run --name $NAME -i -v $ROOTPATH:/test -w /test $IMAGE ${@:2}
+docker run --name $NAME -i -v $ROOTPATH:/test -w /test $IMAGE ${@:2}  --network host
